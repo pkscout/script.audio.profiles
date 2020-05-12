035144431421cda9ff2da8dcebd6cb6aa8de4d2c
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-05-12 11:03:44 -1000

    added support for Pellucid skin

--- resources/lib/apdialog.py
+++ resources/lib/apdialog.py
@@ -19,6 +19,7 @@ SKINVALUESLIST   = { 'default': {'res':'720p', 'diagw':400, 'toph':50, 'bottomh'
                      'skin.embuary-leia': {'res':'1080i', 'diagw':500, 'toph':111, 'bottomh':61, 'buttonh':50},
                      'skin.eminence.2': {'res':'1080i', 'diagw':550, 'toph':82, 'bottomh':20, 'buttonh':82},
                      'skin.ftv': {'res':'1080i', 'diagw':650, 'toph':130, 'bottomh':53, 'buttonh':76},
+                     'skin.pellucid': {'res':'1080i', 'diagw':640, 'toph':108, 'bottomh':20, 'buttonh':72},
                      'skin.quartz': {'res':'720p', 'diagw':480, 'toph':61, 'bottomh':10, 'buttonh':50},
                      'skin.rapier': {'res':'720p', 'diagw':400, 'toph':69, 'bottomh':32, 'buttonh':37}
                    }

ed1141a02a7fc405c61a16ebbf59d67296c3fe07
Fred Rotbart <rotbart@softbart.com>
2023-05-17 16:31:21 +0300

    Added an option to activate a Harmony Hub activity with a profile (if the Kodi Harmony Hub add-on is installed)

--- resources/lib/approfiles.py
+++ resources/lib/approfiles.py
@@ -170,6 +170,13 @@ class Profiles:
                         self.CECCOMMANDS[s_cec]], xbmc.LOGINFO)
             xbmc.executebuiltin(self.CECCOMMANDS[s_cec])
 
+        harmony = self.SETTINGS['profile%s_harmony' % profile]
+        if harmony and harmony != "None":
+            harmony_cmd = "RunScript(script.harmony.control, {})".format(harmony)
+        
+            self.LW.log(['SENDING HARMONY ACTIVITY: %s' % harmony_cmd], xbmc.LOGINFO)
+            xbmc.executebuiltin(harmony_cmd)
+
     def _save(self):
         dialog_return, loglines = apdialog.Dialog().start(self.SETTINGS, title=self.SETTINGS['ADDONLANGUAGE'](32106),
                                                           buttons=self.ENABLEDPROFILES[1], force_dialog=True)
